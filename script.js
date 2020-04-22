1caafa96a178a629bf00d24467dcf18c2b17655c
Bay <b.symonds307@gmail.com>
2020-04-22 13:57:23 +0100

    *more timetable progress, +final UXD pdf, *updated UXD overview

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+jquery(document).ready(function($){
+    $('.time').each(function(){
+        $(this).css({
+            'margin-bottom' : 50 - $(this).outerHeight() + 'px'
+        });
+    });
+});
+
 function toggleBlock(x) {
     x.classList.toggle("available");
     x.classList.toggle("unavailable");
