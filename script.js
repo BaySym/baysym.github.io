4a30f82996002f4884ebe08308496989dd941a0f
Bay <b.symonds307@gmail.com>
2020-04-23 13:03:23 +0100

    +initial tables, +initial style, +initial script

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-jquery(document).ready(function($){
-    $('.time').each(function(){
-        $(this).css({
-            'margin-bottom' : 50 - $(this).outerHeight() + 'px'
-        });
-    });
-});
+window.onload = function(){
+    var all = document.getElementsByTagName("td");
+    for (var i = 0; i < all.length; i++) {
+        if (!all[i].classList.contains("label")) {
+            all[i].onclick = inputClickHandler;
+            all[i].classList.add("free");
+        }
+    }
+};
 
-function toggleBlock(x) {
-    x.classList.toggle("available");
-    x.classList.toggle("unavailable");
+function inputClickHandler(e){
+    e = e||window.event;
+    var tdElm = e.target||e.srcElement;
+    tdElm.classList.toggle("free");
+    tdElm.classList.toggle("busy");
 }
