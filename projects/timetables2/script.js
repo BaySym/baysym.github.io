3c9eb3e27136f9d24038ae85303a4219495063d1
Bay <b.symonds307@gmail.com>
2020-05-12 16:30:17 +0100

    *big brain debugging

--- projects/timetables2/script.js
+++ projects/timetables2/script.js
@@ -1,31 +1,6 @@
-var allCells = [];
+let allTables = [];
+let outputTable;
 
-// Set user input table cells to free, give onclick
-window.onload = function() {
-    // Get the user input table
-    var userTables = document.getElementsByClassName('user-timetable');
-    // Get user cells into an array
-    var theseCells = userTables[0].getElementsByTagName('td');
-    allCells.push(theseCells);
-    // If a cell has no class (it's a time slot), add 'free' and toggle
-    for (var c = 0; c < theseCells.length; c++) {
-        if (theseCells[c].classList.length == 0) {
-            theseCells[c].classList.add('free');
-            theseCells[c].onclick = this.toggleFreeBusy;
-        }
-    }
-    // Get the output table
-    var outputTable = document.getElementById('output-timetable');
-    // Set each cell's innerHTML to 1, if it has no class
-    for (var r = 0; r < outputTable.rows.length; r++) {
-        for (var c = 0; c < outputTable.rows[r].cells.length; c++) {
-            var thisCell = outputTable.rows[r].cells[c];
-            if (thisCell.classList.length == 0) {
-                thisCell.innerHTML = 1;
-            }
-        }
-    }
-}
 
 // Toggle cell free/busy on click
 function toggleFreeBusy() {
@@ -34,44 +9,80 @@ function toggleFreeBusy() {
     this.classList.toggle('busy');
 
     // Adjust the corresponding output cell
-    var thisRow = this.parentElement.rowIndex;
-    var thisCol = this.cellIndex;
-    var outputTable = document.getElementById('output-timetable');
-    var outputCell = outputTable.rows[thisRow].cells[thisCol];
-    if (this.classList.contains('free'))
-        outputCell.innerHTML++;
-    else
-        outputCell.innerHTML--;
+    let thisRow = this.parentElement.rowIndex;
+    let thisCol = this.cellIndex;
+    let outputCell = outputTable.rows[thisRow].cells[thisCol];
+
+    outputCell.innerHTML += this.classList.contains('free') ? 1 : -1;
 }
 
-// Navigate between users
-var currentUser = 0;
-var userCount = 1;
-// Previous user
-function prevUser() {
-    if (currentUser > 0) currentUser--;
-    showUser();
+
+class userTable {
+    constructor(id) {
+        this.table = document.getElementById(id);
+        this.cells = this.table.getElementsByTagName('td');
+        
+        for (let i = 0; i < this.cells.length; i++) {
+            let cell = this.cells[i];
+            // See if cell is a time slot not a label
+            if (cell.classList.length === 0) {
+                cell.classList.add('free');
+                cell.onclick = toggleFreeBusy;
+            }
+        }
+    }
 }
-// Next user
-function nextUser() {
-    if (currentUser < userCount) currentUser++;
-    showUser();
+
+
+// Set user input table cells to free, give onclick
+window.onload = function () {
+    // Add the inital table to allTables
+    allTables.push(new userTable('user1'));
+
+    outputTable = document.getElementById('output-timetable');
+
+    // Get the output table
+    // Set each cell's innerHTML to 1, if it has no class
+    for (let r = 0; r < outputTable.rows.length; r++) {
+        for (let c = 0; c < outputTable.rows[r].cells.length; c++) {
+            let thisCell = outputTable.rows[r].cells[c];
+            if (thisCell.classList.length == 0) {
+                thisCell.innerHTML = 1;
+            }
+        }
+    }
 }
-// Show current user
-function showUser() {
-    var allUsers = document.getElementsByClassName('user');
-    // Hide all users
-    for (user in allUsers) user.style.display = 'none';
-    // Show current user
-    allUsers[currentUser].style.display = 'inline';
-}
+
+
+// // Navigate between users
+// var currentUser = 0;
+// var userCount = 1;
+// // Previous user
+// function prevUser() {
+//     if (currentUser > 0) currentUser--;
+//     showUser();
+// }
+// // Next user
+// function nextUser() {
+//     if (currentUser < userCount) currentUser++;
+//     showUser();
+// }
+// // Show current user
+// function showUser() {
+//     var allUsers = document.getElementsByClassName('user');
+//     // Hide all users
+//     for (let u = 0; u < allUsers.length; u++)
+//         allUsers[u].style.display = 'hidden';
+//     // Show current user
+//     allUsers[currentUser].style.display = 'inline';
+// }
 
 // Add a new user
 function addUser() {
-    var index = document.getElementsByClassName('user').length + 1;
-    var wrapper = document.getElementById('user-wrapper');
+    let index = allTables.length + 1;
+    let wrapper = document.getElementById('user-wrapper');
     // Add user HTML
-    wrapper.innerHTML += `
+    wrapper.insertAdjacentHTML('beforeend', `
     <div class="user" id="user` + index + `">
         <div class="table-header">
             <button onclick="prevUser()">←</button>
@@ -97,30 +108,16 @@ function addUser() {
                 <tr><td class="time-cell-l">16:00</td><td></td><td></td><td></td><td></td><td></td><td class="time-cell-r">16:00</td></tr>
                 <tr><td class="time-cell-l">17:00</td><td></td><td></td><td></td><td></td><td></td><td class="time-cell-r">17:00</td></tr>
         </table>
-    </div>
-    `;
-    // Set cells to free and toggleable
-    var id = 'user' + index;
-    // Get the user input table
-    var newTable = document.getElementById(id);
-    // Get user cells into an array
-    var theseCells = newTable.getElementsByTagName('td');
-    allCells.push(theseCells);
-    // If a cell has no class (it's a time slot), add 'free' and toggle
-    for (var c = 0; c < theseCells.length; c++) {
-        if (theseCells[c].classList.length == 0) {
-            theseCells[c].classList.add('free');
-            theseCells[c].onclick = this.toggleFreeBusy;
-        }
-    }
-    // Get the output table
-    var outputTable = document.getElementById('output-timetable');
-    // Set each cell's innerHTML to 1, if it has no class
-    for (var r = 0; r < outputTable.rows.length; r++) {
-        for (var c = 0; c < outputTable.rows[r].cells.length; c++) {
-            var thisCell = outputTable.rows[r].cells[c];
-            if (thisCell.classList.length == 0) {
-                thisCell.innerHTML ++;
+    </div>`);
+
+    allTables.push(new userTable(`user${index}`));
+
+    // Increase output cells by 1
+    for (let r = 0; r < outputTable.rows.length; r++) {
+        for (let c = 0; c < outputTable.rows[r].cells.length; c++) {
+            let thisCell = outputTable.rows[r].cells[c];
+            if (thisCell.classList.length === 0) {
+                thisCell.innerHTML++;
             }
         }
     }
