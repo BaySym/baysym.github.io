92dcded72a1f273b06adeea65d28a905ae68401d
Bay <bsym307@gmail.com>
2020-08-05 21:21:54 +0100

    *moved timetables, *fixed nav position bug in timetables

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-var themeBtn = document.getElementById('theme-btn');
 var body = document.body;
 var light = true;
 
 function theme() {
     light = !light;
-    
-    if (light) {
-        themeBtn.innerHTML = '☼';
-        body.classList = 'light';
-    }
-    else {
-        themeBtn.innerHTML = '☾';
-        body.classList = 'dark';
-    }
+    body.classList = light ? 'light' : 'dark';
 }
