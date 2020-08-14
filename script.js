2242fba92f9d4c55df51dfeed657302e7a1a862b
Bay <bsym307@gmail.com>
2020-08-14 14:49:35 +0100

    +comments, *optimisations

--- script.js
+++ script.js
@@ -2,28 +2,29 @@ var body = document.body;
 var themeBtn = document.getElementById('theme-btn');
 var social = document.getElementById('social');
 
+// Set the theme to the most recently viewed theme
 setTheme(window.localStorage.getItem('theme'));
 
+// Handle the toggle button, toggling the theme based on local storage
 function theme() {
-    if (window.localStorage.getItem('theme') == 'dark') {
-        setTheme('light');
-    } else {
-        setTheme('dark');
-    }
+    (window.localStorage.getItem('theme') == 'dark') ? setTheme('light') : setTheme('dark');
 }
 
+// Set the page's theme to the specified theme
 function setTheme(theme) {
     if (theme == 'dark') {
+        // Set the theme to dark
         themeBtn.innerHTML = '🌝';
-        themeBtn.borderColor = '#fff';
         if (social) social.style.filter = 'invert()';
-        window.localStorage.setItem('theme', 'dark');
         body.classList = 'dark';
+        // Store the theme
+        window.localStorage.setItem('theme', 'dark');
     } else {
+        // Set the theme to light (default)
         themeBtn.innerHTML = '🌚';
-        themeBtn.borderColor = '#000';
         if (social) social.style.filter = 'none';
-        window.localStorage.setItem('theme', 'light');
         body.classList = 'light';
+        // Store the theme
+        window.localStorage.setItem('theme', 'light');
     }
 }
