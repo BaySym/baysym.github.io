94ea8a3350cde19e494a34b660b528a957d185cb
Bay <bsym307@gmail.com>
2020-08-25 15:09:05 +0100

    *big ol redesign

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
-var body = document.body;
-var themeBtn = document.getElementById('theme-btn');
-var social = document.getElementById('social');
-var caseStudyLink = document.getElementById('case-study-link');
+var items = document.getElementsByClassName('fade-in');
 
-// Set the theme to the most recently viewed theme
-setTheme(window.localStorage.getItem('theme'));
+loadItems();
 
-// Handle the toggle button, toggling the theme based on local storage
-function theme() {
-    (window.localStorage.getItem('theme') == 'dark') ? setTheme('light') : setTheme('dark');
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// Set the page's theme to the specified theme
-function setTheme(theme) {
-    if (theme == 'dark') {
-        // Set the theme to dark
-        themeBtn.innerHTML = '🌝';
-        if (social) social.style.filter = 'invert()';
-        if (caseStudyLink) caseStudyLink.style.filter = 'invert()';
-        body.classList = 'dark';
-        // Store the theme
-        window.localStorage.setItem('theme', 'dark');
-    } else {
-        // Set the theme to light (default)
-        themeBtn.innerHTML = '🌚';
-        if (social) social.style.filter = 'none';
-        if (caseStudyLink) caseStudyLink.style.filter = 'none';
-        body.classList = 'light';
-        // Store the theme
-        window.localStorage.setItem('theme', 'light');
+async function loadItems() {
+    for (var i  = 0; i < items.length; i++) {
+        items[i].style.opacity = 1;
+        await sleep(50);
     }
 }
