4b935eaf4b81fba7b6a8b63b9288ce523ab5e058
Bay <bsym307@gmail.com>
2020-09-24 13:30:11 +0100

    *json rename

---
+++ uni/uni-json.js
@@ -0,0 +1,112 @@
+const json = `
+[
+
+    {
+        "id": "bd",
+        "lectures": [
+            { "title": "Maths for Data Science", "link": "" },
+            { "title": "Introduction to MATLAB", "link": "" },
+            { "title": "Data Preprocessing", "link": "" },
+            { "title": "Data Visualisation", "link": "" },
+            { "title": "Regression", "link": "" },
+            { "title": "Statistical Analysis", "link": "" },
+            { "title": "Classification", "link": "" },
+            { "title": "Clustering", "link": "" },
+            { "title": "Hadoop & MapReduce", "link": "" },
+            { "title": "Parallel Computing", "link": "" }
+        ],
+        "reading": [
+            {
+                "title": "The Data Science Manual",
+                "author": "Skiena",
+                "link": ""
+            },
+            {
+                "title": "Data Science & Big Data Analytics",
+                "author": "EMC Education Services",
+                "link": ""
+            }
+        ]
+    },
+
+    {
+        "id": "ml",
+        "lectures": [],
+        "reading": []
+    },
+
+    {
+        "id": "vr",
+        "lectures": [],
+        "reading": [
+            {
+                "title": "Using the Unity Interface",
+                "author": "Unity Technologies",
+                "link": "https://learn.unity.com/tutorial/using-the-unity-interface"
+            },
+            {
+                "title": "Create Your First Unity Project",
+                "author": "Unity Technologies",
+                "link": "https://learn.unity.com/tutorial/create-your-first-unity-project"
+            },
+            {
+                "title": "Haunted Jaunt: 3D Beginner",
+                "author": "Unity Technologies",
+                "link": "https://learn.unity.com/project/john-lemon-s-haunted-jaunt-3d-beginner"
+            },
+            {
+                "title": "The VR Book",
+                "author": "Jerald",
+                "link": "https://library.lincoln.ac.uk/items/eds/cat04851a/uln.199845?query=title%3A%28The+VR+book%3A+human-centered+design+for+virtual+reality%29&resultsUri=items%3Fquery%3Dtitle%253A%2528The%2BVR%2Bbook%253A%2Bhuman-centered%2Bdesign%2Bfor%2Bvirtual%2Breality%2529%26target%3Deds%26facet%255B0%255D%3Dfulltext%253Ayes&facet%5B0%5D=fulltext%3Ayes&target=eds"
+            }
+        ]
+    },
+
+    {
+        "id": "pp",
+        "lectures": [],
+        "reading": [
+            {
+                "title": "Structured Parallel Programming",
+                "author": "McCool, et al.",
+                "link": ""
+            },
+            {
+                "title": "An Introduction to Parallel Programming",
+                "author": "Pacheco",
+                "link": ""
+            },
+            {
+                "title": "OpenCL in Action",
+                "author": "Scarpino", 
+                "link": ""
+            },
+            {
+                "title": "Heterogeneous Computing with OpenCL 2.0",
+                "author": "Kaeli, et al.",
+                "link": ""
+            }
+        ]
+    },
+
+    {
+        "id": "cp",
+        "lectures": [
+            { "title": "Introduction to the Module", "link": "" },
+            { "title": "User-Centered Design", "link": "" },
+            { "title": "Mobile User Experience", "link": "" },
+            { "title": "Mobile Usability", "link": "" },
+            { "title": "Introduction to React Native", "link": "" },
+            { "title": "Styling and Using the CLI", "link": "" },
+            { "title": "Camera and Interaction", "link": "" },
+            { "title": "Geolocation", "link": "" },
+            { "title": "Ajax API", "link": "" },
+            { "title": "Evaluation Methods", "link": "" },
+            { "title": "Dark Patterns", "link": "" },
+            { "title": "Module Wrap-Up (CRG & Video)", "link": "" }
+        ],
+        "reading": []
+    }
+
+]
+`
